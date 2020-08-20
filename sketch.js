e4f20fc24ca19a659aa9488f8a28f8b242b34060
ilisha0512 <68852416+ilisha0512@users.noreply.github.com>
2020-08-20 15:55:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,57 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var paperball;
+var groundline;
+var dbside1;
+var dbside2;
+var dbside3;
+var dbimage;
+
+function preload()
+{
+ dbimage = loadImage("sprites/dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paperball = new paper (100,200,30,30);
+	groundline = new ground(400,680,800,10);
+	dbside1 = new dustbin(450,670,10,50);
+	dbside2 = new dustbin(475, 675, 50,10);
+  dbside3 = new dustbin (500,670,10,50)
+  dbimage = new dustbin (475, 673, 100,100);
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  paperball.display();
+  groundline.display();
+  dbside1.display();
+  dbside2.display();
+  dbside3.display();
+  dbimage.display();
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+   if (keyCode === UP_ARROW) {
+	   Matter.Body.applyForce(paperball.body, paperball.body.position, {x: 15, y: -15})
+   }
+}
+
